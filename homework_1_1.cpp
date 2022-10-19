ddc0a568778f47fe00de43c84fc455606c22e93c
Denis Smereka <den.smereka@mail.ru>
2022-10-19 22:22:44 +0300

    added first stage of homework_1

---
+++ homework_1_1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    srand(time(nullptr));
+
+    unsigned int dice{}, count_rolls{};
+    unsigned int array_dices[6]{0,0,0,0,0,0};
+
+    std::cout << "Enter count of rolls:" << std::endl;
+    std::cin >> count_rolls;
+    if (count_rolls > 0)
+    {
+        for (int i{}; i < count_rolls; i++)
+        {
+            dice = rand() % 6 + 1;
+            array_dices[dice - 1]++;
+        }
+
+        unsigned int max_value_dices = 0;
+        unsigned int min_value_dices = count_rolls;
+
+        for (int i{}; i < 6; i++)
+        {
+            if (array_dices[i] > max_value_dices)
+                max_value_dices = array_dices[i];
+
+            if (array_dices[i] < min_value_dices)
+                min_value_dices = array_dices[i];
+
+            std::cout << "Number of hits " << i+1 << " - " << array_dices[i] << std::endl;
+        }
+        std::cout << "******************************" << std::endl;
+        std::cout << "Max number of hits" << " - " << max_value_dices << std::endl;
+        std::cout << "Min number of hits" << " - " << min_value_dices << std::endl;
+
+    }
+
+}
