cdeca89bfc8da6bdd153f366c748ca8122e14867
wnkos <wnk248kos@gmail.com>
2022-10-24 11:19:41 +0300

    feat: HomeWork1

---
+++ HW1.2.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int number{};
+	string month[]{ "January", "Fabruary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+	int daysInMonth[]{ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	cin >> number;
+	cout << endl;
+	for (int i = 1; i <= number; i++)
+	{
+		if (number == i)
+		{
+			cout << "Month - " << month[i - 1] << '\n' << "Days in month - " << daysInMonth[i - 1] << endl;
+		}
+		else if (number > 12)
+		{
+			cout << "Month doesn`t exist!" << endl;
+			break;
+		}
+	}
+}
